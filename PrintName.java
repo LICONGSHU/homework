426cef3d811708b19ab743d92a6760aa7d22c058
LICONGSHU <bc254555@student.upm.edu.my>
2025-10-21 15:07:26 +0800

    1

---
+++ PrintName.java
@@ -0,0 +1,12 @@
+package test;
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Name = "LI CONGSHU";
+		System.out.println(Name);
+
+	}
+
+}
